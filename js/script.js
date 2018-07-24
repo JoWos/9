b0cb860189463a18455d4f5f4d74edf7d5806439
Joanna Woszczyk <woszczyk.joanna@gmail.com>
2018-07-24 22:11:10 +0200

    Change styles for top bar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMcontentLoaded', function() {
-	document.querySelectorAll('.js-toggle-sidebar').forEach(function(el){
+	document.querySelectorAll('.js--toggle-sidebar').forEach(function(el){
 		el.addEventListener('click', function(event) {
 		event.preventDefault();
 		document.querySelector('.siedabar').classList.toggle('open')
