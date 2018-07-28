24672ab503f0783779a7e06b8451ac27095be2e5
Joanna Woszczyk <woszczyk.joanna@gmail.com>
2018-07-29 01:12:38 +0200

    Add js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
-document.addEventListener('DOMcontentLoaded', function() {
+/*document.addEventListener('DOMcontentLoaded', function() {
 	document.querySelectorAll('.js--toggle-sidebar').forEach(function(el){
 		el.addEventListener('click', function(event) {
 		event.preventDefault();
 		document.querySelector('.siedabar').classList.toggle('open')
 		})
 	})
-})
+})*/
+// SIDEBAR - OPEN/CLOSE
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.js--toggle-sidebar').forEach(function(el) {
+      el.addEventListener('click', function(event) {
+        event.preventDefault();
+        document.querySelector('.sidebar').classList.toggle('sidebar-open');
+      })
+    });
+  });
 
-
-var hamburger = document.getElementById('hamburger');
-hamburger.addEventListener('click', function() {
-
-	var sidebar = document.getElementById('sidebar');
-	sidebar.classList.toggle('sidebar-open');
-})
 
 // COLLAPSIBLE WALLET //
-var arrow = document.getElementById('wallet-arrow');
+/*var arrow = document.getElementById('wallet-arrow');
 arrow.addEventListener('click', function (){
 
 var wallet = document.getElementById('wallet-box');
@@ -25,4 +27,16 @@ var wallet = document.getElementById('wallet-box');
 var liWallet = document.getElementById('li-wallet');
 	liWallet.classList.toggle('li-wallet-collapsed');
 
-})
+})*/
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.li-wallet').forEach(function(el) {
+      el.addEventListener('click', function(event) {
+        event.preventDefault();
+		document.querySelector('.wallet').classList.toggle('wallet-collapsed');
+		document.querySelector('.icon-arrow-down-small').classList.toggle('hide');
+		document.querySelector('.icon-arrow-up-small').classList.toggle('hide');
+      })
+    });
+  });
