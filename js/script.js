e1b80e04e76232fb27a4882356bfe878c7206387
Joanna Woszczyk <woszczyk.joanna@gmail.com>
2018-07-29 04:24:38 +0200

    Add broken short sidebar

--- js/script.js
+++ js/script.js
@@ -6,12 +6,30 @@
 		})
 	})
 })*/
-// SIDEBAR - OPEN/CLOSE
-document.addEventListener('DOMContentLoaded', function() {
+// SIDEBAR - OPEN/CLOSE - ORIGINAL
+/*document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.js--toggle-sidebar').forEach(function(el) {
       el.addEventListener('click', function(event) {
         event.preventDefault();
         document.querySelector('.sidebar').classList.toggle('sidebar-open');
+      })
+    });
+  });*/
+
+  document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.js--toggle-sidebar').forEach(function(el) {
+      el.addEventListener('click', function(event) {
+		event.preventDefault();
+		var x= window.matchMedia("(min-width: 992px)");
+		if(x.matches) {
+			document.querySelector('.sidebar').classList.toggle('sidebar-short');	
+			document.querySelector('.logo-wrapper').classList.toggle('logo-wrapper-short');
+			document.querySelector('.logo').classList.toggle('logo-short');	
+			document.querySelector('.main-wrapper').classList.toggle('wide');
+		} else {
+		 document.querySelector('.sidebar').classList.toggle('sidebar-open');
+		}
+       
       })
     });
   });
