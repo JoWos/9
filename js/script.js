9d2b3f85cd89ff7a93264cbf1dee5d1a7ab64478
Joanna Woszczyk <woszczyk.joanna@gmail.com>
2018-08-12 18:29:31 +0200

    Add basic modals

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 'use strict'
+
 // SIDEBAR - OPEN/CLOSE 
 document.addEventListener('DOMContentLoaded', function() {
 	document.querySelectorAll('.js--toggle-sidebar').forEach(function(el) {
@@ -28,3 +29,50 @@ document.addEventListener('DOMContentLoaded', function() {
 		})
 	});
 });
+
+
+
+////////MODALS
+
+var showModal = function(event){
+	event.preventDefault();		
+	var modals = document.querySelectorAll('.modal');
+		 
+	for (var i = 0; i < modals.length; i++) {
+		modals[i].classList.remove('show');
+}
+
+	var href = this.getAttribute('href');
+	document.querySelector(href).classList.add('show')
+	document.querySelector('#modal-overlay').classList.add('show');
+};
+
+//przypisanie funkcji otwierającej do guzików
+var modalLinks = document.querySelectorAll('.show-modal');
+for(var i = 0; i < modalLinks.length; i++){
+	modalLinks[i].addEventListener('click', showModal);
+}
+	
+//ZAMYKANIE MODALA
+var hideModal = function(event){
+	event.preventDefault();
+	document.querySelector('#modal-overlay').classList.remove('show');
+};
+
+//przypisanie funkcji zamykającej do guzików
+var closeButtons = document.querySelectorAll('.modal .close');
+for(var i = 0; i < closeButtons.length; i++){
+	closeButtons[i].addEventListener('click', hideModal);
+}
+
+
+document.querySelector('#modal-overlay').addEventListener('click', hideModal);
+//uniemożliwienie zamykania modala poprzez kliknięcie w samego modala
+var modals = document.querySelectorAll('.modal');
+
+for(var i = 0; i < modals.length; i++){
+	modals[i].addEventListener('click', function(event){
+		event.stopPropagation();
+	});
+}
+
