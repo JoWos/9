13ba8e0d05e278119e663f06697d16f4af611a6d
Joanna Woszczyk <woszczyk.joanna@gmail.com>
2018-08-27 17:39:09 +0200

    Add chart.js

--- js/script.js
+++ js/script.js
@@ -72,3 +72,43 @@ for(var i = 0; i < modals.length; i++){
 	});
 }
 
+//WYKRES
+var ctx = document.getElementById('myChart').getContext('2d');
+
+Chart.scaleService.updateScaleDefaults('linear', {
+    ticks: {
+        max: 600
+    }
+});
+var chart = new Chart(ctx, {
+    // 1
+    type: 'bar',
+    data: {
+        // 2
+        labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+        // 3
+        datasets: [{
+            // 4
+            label: "Signups",
+            // 5
+            backgroundColor: '#8DBEC8',
+            borderColor: '#8DBEC8',
+            // 6
+            data: [ 352, 200, 220, 400, 350, 400, 372, 309, 321, 388 ],
+        },
+        {
+            label: "FTD",
+            backgroundColor: '#F29E4E',
+            borderColor: '#F29E4E',
+            data: [ 406, 172, 301, 280, 447, 111, 150, 244, 463, 326 ],
+        },
+        {
+            label: "Earned",
+            backgroundColor: '#71B374',
+            borderColor: '#71B374',
+            data: [ 159, 249, 168, 390, 267, 141, 213, 238, 248, 148 ],
+            // 7
+            hidden: true,
+        }]
+    },
+}); 
